5cb594fcbc8f28dc0f9c56154e827624345c8ed8
Joy <mdjoynalabedin2003@gmail.com>
2025-07-04 13:58:33 +1000

    added my hyprland config file such as hyprland config file, hypridle file, hyprlock file and swaybar, neovim and fish as well

---
+++ config/fish/config.fish
@@ -0,0 +1,46 @@
+set fish_greeting
+
+if status is-interactive
+    set -g fish_key_bindings fish_vi_key_bindings
+    function fish_mode_prompt; end
+end
+
+if test -e "$HOME/.config/fish/local.fish"
+    source "$HOME/.config/fish/local.fish"
+end
+
+# exports
+export FZF_DEFAULT_OPTS="--layout=reverse --height=30%"
+export MANPAGER='nvim +Man!'
+export ANDROID_HOME="$HOME/.local/android-sdk"
+
+
+# PATH
+# export GOPATH="$HOME/.local/go"
+# export GOROOT="$HOME/.local/go"
+export PATH="$PATH:$HOME/.local/go/bin"
+export PATH="$PATH:$HOME/.local/bin"
+export PATH="$PATH:$ANDROID_HOME/cmdline-tools/latest/bin/"
+
+alias gd='pwd | wl-copy'
+alias g="git"
+alias ga="git add"
+alias gm="git commit -m"
+alias gp="git push"
+alias gc="git clone"
+alias grep="grep --color"
+alias l='lsd'
+alias ll="lsd -al"
+alias ff="fastfetch"
+alias bl="bluetoothctl"
+alias wlc="wl-copy"
+alias wlp="wl-paste"
+alias code="codium"
+alias mpva='mpv --script-opts=ytdl_hook-ytdl_path=yt-dlpn'
+alias mpv='mpv --save-position-on-quit --resume-playback'
+alias tm='tmux a || tmux'
+alias z='zathura --fork'
+alias rsync='rsync --progress'
+
+# Set up fzf key bindings
+fzf --fish | source
---
+++ config/nvim/lua/wizsk/init.lua
@@ -0,0 +1,65 @@
+require("wizsk.set")
+require("wizsk.remap")
+require("wizsk.lazy")
+require("wizsk.plugins.treesitter")
+require("wizsk.plugins.mason")
+require("wizsk.plugins.telescope")
+require("wizsk.plugins.lsp")
+-- require("wizsk.plugins.zenmode")
+require("wizsk.plugins.harpoon")
+require("wizsk.plugins.undotree")
+
+local augroup = vim.api.nvim_create_augroup
+local wizskGroup = augroup('wizskGroup', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+
+
+
+-- reload suff
+-- pore dehum ni kita kore
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd({ "BufWritePre" }, {
+    group = wizskGroup,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
+
+autocmd({ "BufWritePre" }, {
+    group = wizskGroup,
+    pattern = "*",
+    callback = function()
+        vim.lsp.buf.format()
+    end,
+})
+
+autocmd({ "BufWritePre" }, {
+    pattern = "*.go",
+    callback = function()
+        require("go.format").goimport()
+    end,
+})
+
+
+
+
+-- netrw style change
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
